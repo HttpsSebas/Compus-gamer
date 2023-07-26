d0273f60bfe72dda7028c0178a4a65ce98023e8c
HttpsSebas <130616272+HttpsSebas@users.noreply.github.com>
2023-07-26 17:07:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,77 @@ submenuItems.forEach(item => {
     submenu.classList.toggle('active');
   });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+  addToCartButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const nombreProducto = button.dataset.product;
+      const precioProducto = parseFloat(button.dataset.price);
+
+      let productosEnCarrito = localStorage.getItem('productosEnCarrito');
+      if (!productosEnCarrito) {
+        productosEnCarrito = [];
+      } else {
+        productosEnCarrito = JSON.parse(productosEnCarrito);
+      }
+
+      productosEnCarrito.push({ nombre: nombreProducto, precio: precioProducto });
+      localStorage.setItem('productosEnCarrito', JSON.stringify(productosEnCarrito));
+
+      alert('Producto agregado al carrito.');
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const cartItemsContainer = document.querySelector('.cart-items');
+  const totalPriceElement = document.getElementById('total-price');
+
+  // Simulación de productos agregados al carrito
+  const cartItems = [
+    {
+      id: 1,
+      name: '16" Nova - RTX 4070',
+      price: 1999,
+      image: 'images/notebook1.jpg'
+    },
+    {
+      id: 2,
+      name: '17" Avon - RTX 4080/4090',
+      price: 2864,
+      image: 'images/notebook2.jpg'
+    },
+    // Agrega más productos aquí...
+  ];
+
+  function updateCartTotal(cartItems) {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.price;
+    });
+    totalPriceElement.textContent = `$${total.toFixed(2)}`;
+  }
+
+  function renderCartItem(item) {
+    const itemElement = document.createElement('div');
+    itemElement.classList.add('item');
+    itemElement.innerHTML = `
+      <img src="${item.image}" alt="${item.name}" height="50">
+      <p>${item.name}</p>
+      <p>$${item.price.toFixed(2)}</p>
+    `;
+    return itemElement;
+  }
+
+  function updateCart(cartItems) {
+    cartItemsContainer.innerHTML = '';
+    cartItems.forEach((item) => {
+      const itemElement = renderCartItem(item);
+      cartItemsContainer.appendChild(itemElement);
+    });
+    updateCartTotal(cartItems);
+  }
+
+  updateCart(cartItems);
+});
